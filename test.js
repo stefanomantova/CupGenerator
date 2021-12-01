aec8410b59d8b3fb0bcf8aaed0d622448750c61f
stefanomantova <stefano4d@gmail.com>
2021-11-30 21:21:34 -0300

    Added generate team names
    Need to figure out the best way to register the names to the created inputs in new variables
    Array? Objects?

--- test.js
+++ test.js
@@ -1,19 +1,12 @@
 var numberTeams = document.getElementById("numberTeams")
 var team = []
-
 function generateFields(){
     var numberOfTeams = parseInt(numberTeams.value)
-    var input = []
-for(let cont = 0; cont < numberOfTeams; cont++){
-    input  = document.createElement("input")
+    for(let cont = 0; cont < numberOfTeams ; cont++){
+    var input = document.createElement("input")
     input.type = 'text'
-
-   // input[cont].setAttribute('type', 'text')
-    //input[cont].setAttribute('id',`team${team[numberOfTeams]}` ) 
     var teamsDiv = document.getElementById("teamsDiv")
     teamsDiv.appendChild(input)
-
 }
-
+alert(numberOfTeams)
 }
-
