82aedc648da7beadb2102da238128bb6a7109176
Stéfano Mantovani <stefano4d@gmail.com>
2021-12-10 20:37:04 -0300

    Reset to Eliminate Teams from Array

--- scriptlogic.js
+++ scriptlogic.js
@@ -1,6 +1,5 @@
 var numberTeams = document.getElementById("numberTeams")
 var teamStorage = []
-
 function generateFields(){
     
     var numberOfTeams = parseInt(numberTeams.value)
@@ -23,7 +22,6 @@ var confirmStart = document.createElement("input")
 confirmStart.type = 'button'
 confirmStart.value = 'Start!'
 teamsDiv.appendChild(confirmStart)
-
 confirmStart.onclick = function(){
     var check = new Boolean(true)
     for(let i in inputTeams){
@@ -34,14 +32,11 @@ confirmStart.onclick = function(){
     if(!check){
     alert('Preencha todos os campos')
     }else{
-    setTeamStorage(inputTeams)   
-     
-    knockoutPhase(inputTeams) //Aqui vai a chamada para a função que inicia a Cup. Inicialmente, apenas a chamada para knockout round. Novas implementações de formatos (league, group cup, etc) podem ser realizadas posteriormente e chamadas aqui
+    knockoutPhase(inputTeams,1) //Aqui vai a chamada para a função que inicia a Cup. Inicialmente, apenas a chamada para knockout round. Novas implementações de formatos (league, group cup, etc) podem ser realizadas posteriormente e chamadas aqui
     }
 }
 }
 }
-
 function createTeam(name, id, matchScore, currentPoints){
     this.name = name
     this.id - id
@@ -49,24 +44,18 @@ function createTeam(name, id, matchScore, currentPoints){
     this.currentPoints = currentPoints
     
 }
-
-function setTeamStorage(arrayInput){
-    for(let j in arrayInput){
-        teamStorage[j] = new createTeam(arrayInput[j].value,j)    
-        
-    }
-}
-
-
-function knockoutPhase(){
+function knockoutPhase(previousTeams, roundCount){
+    removeAllChildNodes(teamsDiv)
     document.getElementById("title").innerHTML = 'Set the Score for the current phase!'
-    while(teamStorage.length > 1){
-        removeAllChildNodes(teamsDiv)
-        setRound(teamStorage)
-
-    }
-    alert("Campeão é "+ teamStorage[0].name)
+    if(roundCount == 1){
+for(let j in previousTeams){
+    teamStorage[j] = new createTeam(previousTeams[j].value,j)    
+    
 }
+    }
+    setRound(teamStorage)
+}
+
 
 
 function removeAllChildNodes(parent) {
@@ -74,7 +63,6 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild)
     }
 }
-
 function matchResult(team1, team2){
     if(team1.matchScore > team2.matchScore){
         return team1
@@ -82,7 +70,6 @@ function matchResult(team1, team2){
         return team2
     }
 }
-
 function setRound(teams){
     var match = []
     var score = []
@@ -98,13 +85,14 @@ for(let i = 0; i < teams.length; i+=2){
     score[i+1].className = 'score'
     score[i].style.width = "32px"   
     score[i+1].style.width = "32px" 
-
     match[i].innerHTML = `${teamStorage[i].name} `
     match[i].appendChild(score[i])
     match[i].innerHTML += ` X `
     match[i].appendChild(score[i+1])
     match[i].innerHTML += ` ${teamStorage[i+1].name}`
-    
+
+
+
 }
 
 var buttonNextLine = document.createElement('p')
@@ -113,21 +101,21 @@ buttonNext.type = 'button'
 buttonNext.value = 'Next round!'
 teamsDiv.appendChild(buttonNextLine)
 buttonNextLine.appendChild(buttonNext)
-
 buttonNext.onclick = function processRound(){
-
     
     for(let j = 0; j < teamStorage.length; j++){
         if(!score[j]){
             alert("Empty Score!")
         }else{
         teamStorage[j].matchScore = parseInt(document.getElementsByClassName('score')[j].value)
+        //eliminateTeamsFromArray()
         let nextRoundTeams = []
-           
+
         }
-        
+
     }
     eliminateTeamsFromArray()
+
       
 }
 }
@@ -135,8 +123,8 @@ buttonNext.onclick = function processRound(){
 function eliminateTeamsFromArray(){
     for(let i = 0,  j = 0; i < teamStorage.length; i+=2, j++){
         teamStorage[j] = matchResult(teamStorage[i], teamStorage[i+1])            //Passar o nome dos times "vencedores" pro array e repassar pro array principal pro proximo round começar
-        
+        alert(teamStorage[j].name)
+
      }
      teamStorage.splice(teamStorage.length/2, teamStorage.length/2)
-
 }
